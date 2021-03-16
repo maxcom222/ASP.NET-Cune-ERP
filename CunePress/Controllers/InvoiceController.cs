a3ba2fec31ac6b2919da457365dda9666566fc4d
kcaribou <kcaribou0428@gmail.com>
2021-03-16 23:07:12 +0800

    Customer Home, View required Book

---
+++ CunePress/Controllers/InvoiceController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CunePress.Controllers
+{
+    [AllowAnonymous]
+    public class InvoiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
